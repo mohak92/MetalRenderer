e530dc024fc405d93f84e619d23cbf570a8a250e
mohak92 <tamhane.mohak@gmail.com>
2025-04-27 17:12:59 -0400

    setup metal in swift

---
+++ MetalRenderer/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  MetalRenderer
+//
+//  Created by Mohak Tamhane on 4/27/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ MetalRenderer/Renderer.swift
@@ -0,0 +1,67 @@
+//
+//  Renderer.swift
+//  MetalRenderer
+//
+//  Created by Mohak Tamhane on 4/27/25.
+//
+
+import Foundation
+import MetalKit
+
+class Renderer: NSObject {
+    static var device: MTLDevice!
+    let commandQueue: MTLCommandQueue!
+    static var library: MTLLibrary!
+    let pipelineState: MTLRenderPipelineState!
+    
+    init(view: MTKView) {
+        guard let device = MTLCreateSystemDefaultDevice(),
+        let commandQueue = device.makeCommandQueue() else {
+            fatalError("Unable to connect to GPU")
+        }
+        Renderer.device = device
+        self.commandQueue = commandQueue
+        Renderer.library = device.makeDefaultLibrary()!
+        pipelineState = Renderer.createPipelineState()
+        super.init()
+    }
+    
+    static func createPipelineState() -> MTLRenderPipelineState {
+        let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
+        
+        //pipeline state properties
+        pipelineStateDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+        let vertexFunction = Renderer.library.makeFunction(name: "vertex_main")
+        let fragentFunction = Renderer.library.makeFunction(name: "fragment_main")
+        pipelineStateDescriptor.vertexFunction = vertexFunction
+        pipelineStateDescriptor.fragmentFunction = fragentFunction
+        
+        return try! Renderer.device.makeRenderPipelineState(descriptor: pipelineStateDescriptor)
+    }
+    
+}
+
+extension Renderer: MTKViewDelegate {
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        
+    }
+    
+    func draw(in view: MTKView) {
+        guard let commandBuffer = commandQueue.makeCommandBuffer(),
+            let drawable = view.currentDrawable,
+            let descriptor = view.currentRenderPassDescriptor,
+            let commandEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: descriptor) else {
+            return
+        }
+        
+        commandEncoder.setRenderPipelineState(pipelineState)
+        // draw
+        commandEncoder.drawPrimitives(type: .point,
+                                      vertexStart: 0,
+                                      vertexCount: 1)
+        commandEncoder.endEncoding()
+        
+        commandBuffer.present(drawable)
+        commandBuffer.commit()
+    }
+}
